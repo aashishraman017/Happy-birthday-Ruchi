014f724fdfab6b284779cc569749baac06a12680
Aashu <168896972+aashishraman017@users.noreply.github.com>
2026-01-30 13:12:09 +0530

    Add more photos to the PhotosScreen component

--- components/screens/PhotosScreen.jsx
+++ components/screens/PhotosScreen.jsx
@@ -14,8 +14,12 @@ const photos = [
   { src: "/1.jpeg" }, 
   { src: "/2.jpeg" },
   { src: "/3.jpeg" },
-  { src: "/4.jpeg" }, 
-  { src: "/5.jpeg" }
+  { src: "/4.jpeg" },
+  { src: "/5.jpeg" },
+  { src: "/6.jpeg" },
+  { src: "/7.jpeg" },
+  { src: "/8.jpeg" },
+  { src: "/9.jpeg" }
 ]
 
 export default function PhotosScreen({ onNext }) {
